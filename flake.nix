ade69c21ac8cbc40a7799b375c0052e17d552740
0WN463 <>
2025-07-01 00:09:13 +0800

    Initial nix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "dev-environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in with pkgs; rec {
+        # Development environment
+        devShell = mkShell {
+          name = "dev-shell";
+          nativeBuildInputs = [ nodejs typescript-language-server tailwindcss-language-server ];
+        };
+
+        # The default package when a specific package name isn't specified.
+        defaultPackage = packages.app;
+      }
+    );
+}
