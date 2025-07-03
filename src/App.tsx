3da85df23eae23531a9f7e34a96d2ab59d3b29c9
0WN463 <>
2025-07-04 01:05:30 +0800

    Run prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import { Chess, DEFAULT_POSITION } from 'chess.js';
-import { Chessboard } from 'react-chessboard';
+import { Chess, DEFAULT_POSITION } from "chess.js";
+import { Chessboard } from "react-chessboard";
 
 import yaml from "js-yaml";
 
@@ -30,119 +30,149 @@ e4 e5 Nf3 Nc6 c3:
         - c6 dxc6:
           - dxe5 cxb7+ Bd7 bxa8Q
           - bxc6 Nxc6 Nxc6 Bxc6+ Bd7 Bxe4 Qe7 O-O Qxe4 Re1
-`
+`;
 
-type Tree = {value: string, children: Tree[]};
-type RootedTree = {children: Tree[]};
+type Tree = { value: string; children: Tree[] };
+type RootedTree = { children: Tree[] };
 
 const strToTree = (s: string, initialChildren?: Tree[]) => {
-	return s.split(" ").toReversed().reduce((acc: Tree | null, v: string) => !acc ? {value: v, children: initialChildren} : {value: v, children: [acc]}, null)
-}
+  return s
+    .split(" ")
+    .toReversed()
+    .reduce(
+      (acc: Tree | null, v: string) =>
+        !acc
+          ? { value: v, children: initialChildren }
+          : { value: v, children: [acc] },
+      null,
+    );
+};
 
 const makeRootedTree = (s: Object) => {
-	if (!s)
-		return null
-	const tree = makeTree(s);
+  if (!s) return null;
+  const tree = makeTree(s);
 
-	return {children: [tree]};
-}
+  return { children: [tree] };
+};
 
 const makeTree = (s: Object) => {
-	if (typeof s === 'string') {
-		return strToTree(s);
-	}
-	const k = Object.keys(s)[0];
+  if (typeof s === "string") {
+    return strToTree(s);
+  }
+  const k = Object.keys(s)[0];
 
-	const children = Object.values(s)[0].map(makeTree);
+  const children = Object.values(s)[0].map(makeTree);
 
-	const tree = strToTree(k, children);
+  const tree = strToTree(k, children);
 
-	return tree;
-}
+  return tree;
+};
 
 const moveToCoords = (position: string, move: string) => {
-	const chess = new Chess(position);
+  const chess = new Chess(position);
 
-	try {
-		const {to, from}= chess.move(move.replace("?", ""));
-		return {startSquare: from, endSquare: to, isBlunder: move.includes("?")};
-	} catch {
-		console.log("invalid move", move, position, DEFAULT_POSITION);
-	}
-}
+  try {
+    const { to, from } = chess.move(move.replace("?", ""));
+    return { startSquare: from, endSquare: to, isBlunder: move.includes("?") };
+  } catch {
+    console.log("invalid move", move, position, DEFAULT_POSITION);
+  }
+};
 
 const loadYaml = (s: string) => {
-	try {
-		return yaml.load(s);
-	}
-	catch {
-		console.log("invalid yaml")
-		return null;
-	}
-}
+  try {
+    return yaml.load(s);
+  } catch {
+    console.log("invalid yaml");
+    return null;
+  }
+};
 
 const App = () => {
-	const [lines, setLines] = useState(ponziani);
+  const [lines, setLines] = useState(ponziani);
 
-	const yaml = loadYaml(lines);
-	const [history, setHistory] = useState([{
-		tree: makeRootedTree(yaml),
-		position: DEFAULT_POSITION,
-	}]);
+  const yaml = loadYaml(lines);
+  const [history, setHistory] = useState([
+    {
+      tree: makeRootedTree(yaml),
+      position: DEFAULT_POSITION,
+    },
+  ]);
 
-	if (!yaml)
-	return (
-		<main className="w-4/5">
-		<div className="flex gap-6">
-		<Chessboard key="invalid"/>
-		<textarea key="invalid-text" className="w-full bg-red-300" value={lines} onChange={e => setLines(e.target.value)}/>
-		</div>
-		</main>
-	)
+  if (!yaml)
+    return (
+      <main className="w-4/5">
+        <div className="flex gap-6">
+          <Chessboard key="invalid" />
+          <textarea
+            key="invalid-text"
+            className="w-full bg-red-300"
+            value={lines}
+            onChange={(e) => setLines(e.target.value)}
+          />
+        </div>
+      </main>
+    );
 
-	const {tree, position} = history.at(-1);
+  const { tree, position } = history.at(-1);
 
-	let moves = tree.children?.map(child => moveToCoords(position, child.value));
+  let moves = tree.children?.map((child) =>
+    moveToCoords(position, child.value),
+  );
 
-	const options =  {
-		arrows: moves?.map(m => ({ ...m, color: m.isBlunder ? "red" : "green"})) ?? [],
-		position,
-	}
+  const options = {
+    arrows:
+      moves?.map((m) => ({ ...m, color: m.isBlunder ? "red" : "green" })) ?? [],
+    position,
+  };
 
-	const onMoveClicked = (index: number) => {
-		const chess = new Chess(position);
-		if (tree?.children.length) {
-			chess.move(tree.children[index].value.replace("?", ""));
-			setHistory([
-				...history, 
-				{tree: tree.children[index], position: chess.fen()},
-			]);
-		}
-	}
+  const onMoveClicked = (index: number) => {
+    const chess = new Chess(position);
+    if (tree?.children.length) {
+      chess.move(tree.children[index].value.replace("?", ""));
+      setHistory([
+        ...history,
+        { tree: tree.children[index], position: chess.fen() },
+      ]);
+    }
+  };
 
-	const onBack = () => {
-		setHistory(history.slice(0, -1));
-	}
+  const onBack = () => {
+    setHistory(history.slice(0, -1));
+  };
 
-	return (
-		<main className="w-3/5">
-		<div className="flex gap-6 mb-6">
-		<Chessboard
-		key="valid"
-		options={options}
-		/>
-		<textarea key="valid-text" className="w-full" value={lines} onChange={e => setLines(e.target.value)}/>
-		</div>
-		<div className="flex w-full gap-3">
-		{
-			tree.children?.map((c, i) => 		
-			   <button key={i} className="border-4 rounded p-2 basis-0 grow max-w-xs" onClick={() => onMoveClicked(i)} >{c.value}</button>
-		  )
-		}
-		{history.length > 1 && <button className="border-4 p-2 basis-0 grow max-w-xs" onClick={onBack} >Back</button>}
-		</div>
-		</main>
-	)
-}
+  return (
+    <main className="w-3/5">
+      <div className="flex gap-6 mb-6">
+        <Chessboard key="valid" options={options} />
+        <textarea
+          key="valid-text"
+          className="w-full"
+          value={lines}
+          onChange={(e) => setLines(e.target.value)}
+        />
+      </div>
+      <div className="flex w-full gap-3">
+        {tree.children?.map((c, i) => (
+          <button
+            key={i}
+            className="border-4 rounded p-2 basis-0 grow max-w-xs"
+            onClick={() => onMoveClicked(i)}
+          >
+            {c.value}
+          </button>
+        ))}
+        {history.length > 1 && (
+          <button
+            className="border-4 p-2 basis-0 grow max-w-xs"
+            onClick={onBack}
+          >
+            Back
+          </button>
+        )}
+      </div>
+    </main>
+  );
+};
 
-export default App
+export default App;
